61d2105931cbed4397a947432a0255059706b63e
dabhandgroup <49932957+dabhandgroup@users.noreply.github.com>
2023-04-11 17:29:49 +1000

    3

--- pages/_app.js
+++ pages/_app.js
@@ -12,3 +12,23 @@ function Application({ Component, pageProps }) {
 }
 
 export default Application
+
+ 
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+ 
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+ 
+storyblokInit({
+  accessToken: "your-preview-token",
+  use: [apiPlugin],
+  components,
+});
